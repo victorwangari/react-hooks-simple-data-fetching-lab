10a66b66a889ee5e4e119b03f844f9bf03090caa
Victor kamau <Victor Wangari>
2024-04-30 20:58:21 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React from "react";
+import {useState ,useEffect } from "react";
+
+function App(){
+    const [data,setData] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            setData(data.message)
+            setIsLoading(false)
+
+        })
+    },[])
+    return(
+        <>
+        {isLoading ? (
+            <p>Loading...</p>
+        ):(
+            <>  
+            {data && <img src={data} alt="A Random Dog" />}</>
+        ) }
+      
+        </>
+    )
+}
+export default App
